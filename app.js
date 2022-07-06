48d0e12d015223d1adb64335d7057a12ca847961
Strike2-ux <13paullevy@gmail.com>
2022-07-06 19:17:28 -0400

    build: first chunk

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// motor de plantilla
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.use(express.static( __dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.render("index", {titulo: "mi titulo dinámico"})
+})
+
+app.get('/servicios', (req, res) => {
+    res.render("servicios", {tituloServicios: "Este es un mensaje dinámico de servicios"})
+})
+
+
+
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
+
+app.use((req, res, next) => {
+    res.status(404).render("404", { 
+        titulo: "404",
+        descripcion: "Título del sitio web"
+})
+})
---
+++ frutas.js
@@ -0,0 +1,7 @@
+const frutas = ['platano', 'manzana', 'banana', 'pera'];
+const dinero = 1000;
+
+module.exports = {
+    frutas: frutas,
+    dinero: dinero,
+}
